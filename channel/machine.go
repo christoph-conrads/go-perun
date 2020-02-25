123055aa1aeaf7800d5f70fdcbe7b35b942b783a
Oliver Tale-Yazdi <oliver@perun.network>
2020-02-25 15:50:09 +0100

    :sparkles: [channel] Made machine cloneable

--- channel/machine.go
+++ channel/machine.go
@@ -57,7 +57,7 @@ var signingPhases = []Phase{InitSigning, Signing}
 // individually.
 type machine struct {
 	phase     Phase
-	acc       wallet.Account
+	acc       wallet.Account `cloneable:"shallow"`
 	idx       Index
 	params    Params
 	stagingTX Transaction
@@ -65,7 +65,7 @@ type machine struct {
 	prevTXs   []Transaction
 
 	// log is a fields logger for this machine
-	log log.Logger
+	log log.Logger `cloneable:"shallow"`
 }
 
 // NewMachine returns a new uninitialized machine for the given parameters.
@@ -360,4 +360,22 @@ func (m *machine) phaseErrorf(expected PhaseTransition, format string, args ...i
 // selfTransition returns a PhaseTransition from current to current phase.
 func (m *machine) selfTransition() PhaseTransition {
 	return PhaseTransition{m.phase, m.phase}
+}
+
+func (m *machine) Clone() *machine {
+	var clonedPrevTx []Transaction
+	for i := range m.prevTXs {
+		clonedPrevTx = append(clonedPrevTx, *m.prevTXs[i].Clone())
+	}
+
+	return &machine{
+		phase:     m.phase,
+		acc:       m.acc,
+		idx:       m.idx,
+		params:    *m.params.Clone(),
+		stagingTX: *m.stagingTX.Clone(),
+		currentTX: *m.currentTX.Clone(),
+		prevTXs:   clonedPrevTx,
+		log:       m.log,
+	}
 }
